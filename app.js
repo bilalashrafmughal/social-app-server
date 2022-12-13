1a50834d4d2f699d21e67f00e40c1a0f019bf607
Bilal Ashraf <bilalashraf6233@gmail.com>
2022-12-14 01:02:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const Post = require("./models/post");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// bring in routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const post = require("./models/post");
+// apiDocs
+app.get("/api", (req, res) => {
+  fs.readFile("docs/apiDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err,
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
+
+// middleware -
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.get("/api/test", async (req, res) => {
+  const posts = await Post.find();
+  res.json({ message: posts });
+});
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized!" });
+  }
+});
+
+// Deployment Setting...
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`A Node Js API is listening on port: ${port}`);
+});
