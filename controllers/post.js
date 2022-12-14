24815df75644d946d30990aab67f54076c4fe1c4
Bilal Ashraf <bilalashraf6233@gmail.com>
2022-12-14 16:25:19 +0500

    content type is set

--- controllers/post.js
+++ controllers/post.js
@@ -192,7 +192,6 @@ exports.deletePost = (req, res) => {
 
 exports.photo = (req, res, next) => {
   res.set("Content-Type", req.post.photo.contentType);
-  console.log("sending photo...");
   return res.send(req.post.photo.data);
 };
 
